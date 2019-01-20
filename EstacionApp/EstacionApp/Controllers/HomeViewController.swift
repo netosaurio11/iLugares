8f61636b7c33f550957c18c4235063bae882035f
CobainJosue <chivasagrada@live.com.mx>
2019-01-19 21:39:10 -0600

    HomeViewController added

---
+++ EstacionApp/EstacionApp/Controllers/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  EstacionApp
+//
+//  Created by Josue Quiñones on 1/19/19.
+//  Copyright © 2019 Ernesto Daniel Mejia Valdiviezo. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
