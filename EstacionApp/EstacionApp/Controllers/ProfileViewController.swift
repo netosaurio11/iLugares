0a958cc79431162636431389a962419316c36088
CobainJosue <chivasagrada@live.com.mx>
2019-01-24 15:20:06 -0600

    Keyboard adjust for ProfileViewController

--- EstacionApp/EstacionApp/Controllers/ProfileViewController.swift
+++ EstacionApp/EstacionApp/Controllers/ProfileViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var constraintBottomScroll: NSLayoutConstraint!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    
+    var user: User?
 
+    override func viewWillAppear(_ animated: Bool) {
+        //Observers para el teclado
+        NotificationCenter.default.addObserver(self, selector: #selector(self.adjustForKeyboard(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.adjustForKeyboard(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self)
+        super.viewWillDisappear(animated)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
     }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        let userInfo = notification.userInfo!
+        
+        let keyboardScreenEndFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            scrollView.contentInset = UIEdgeInsets.zero
+        } else {
+            scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
+        }
+        
+        scrollView.scrollIndicatorInsets = scrollView.contentInset
+        
+//        let selectedRange = scrollView.selectedRange
+//        scrollView.scrollRangeToVisible(selectedRange)
+    }
+    
     
     
     @IBAction func closeSession(_ sender: UIButton) {
@@ -22,16 +60,15 @@ class ProfileViewController: UIViewController {
         
         persistent.deleteSession()
         self.performSegue(withIdentifier: "unwindToViewController1", sender: self)
-//        let rootVC: UIViewController?
-//        
-//        rootVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "initialVC") as! InitialViewController
-//        
-//        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-//        
-//        appDelegate.window?.rootViewController = rootVC
-        
-        //navigationController?.popToViewController(self.navigationController?.viewControllers[0] as! InitialViewController, animated: true)
-        
     }
+    
+    @IBAction func tapCloseKeyboard(_ sender: UITapGestureRecognizer) {
+        
+        //Terminar edición
+        self.view.endEditing(true)
+    }
+    
+    
+    
 
 }
