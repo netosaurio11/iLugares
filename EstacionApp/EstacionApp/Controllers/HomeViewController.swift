4c6c8c47c94346fb05fb3ad591d6ab7fcc7edc4a
Ernesto Daniel Mejia Valdiviezo <netosaurio11@MacBook-Air-de-Ernesto.local>
2019-01-28 17:39:13 -0600

    Se implementa TableView de busqueda de direcciones al flujo de registro

--- EstacionApp/EstacionApp/Controllers/HomeViewController.swift
+++ EstacionApp/EstacionApp/Controllers/HomeViewController.swift
@@ -47,7 +47,7 @@ class HomeViewController: UIViewController, UISearchBarDelegate, HandleMapSearch
         annotation.title = placemark.name
         if let city = placemark.locality,
             let state = placemark.administrativeArea {
-            annotation.subtitle = "(city) (state)"
+            annotation.subtitle = "\(city) \(state)"
         }
         mapView.addAnnotation(annotation)
         let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
@@ -71,60 +71,6 @@ class HomeViewController: UIViewController, UISearchBarDelegate, HandleMapSearch
         present(searchController, animated: true, completion: nil)
         
     }
-    
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        //Ignoring user
-        UIApplication.shared.beginIgnoringInteractionEvents()
-        
-        //Activity Indicator
-        let activityIndicator = UIActivityIndicatorView()
-        activityIndicator.style = UIActivityIndicatorView.Style.gray
-        activityIndicator.center = self.view.center
-        activityIndicator.hidesWhenStopped = true
-        activityIndicator.startAnimating()
-        
-        self.view.addSubview(activityIndicator)
-        
-        searchBar.resignFirstResponder()
-        dismiss(animated: true, completion: nil)
-        
-        //Request
-        let searchBarRequest = MKLocalSearch.Request()
-        searchBarRequest.naturalLanguageQuery = searchBar.text
-        
-        let activeSearch = MKLocalSearch(request: searchBarRequest)
-        
-        activeSearch.start { (response, error) in
-            activityIndicator.stopAnimating()
-            UIApplication.shared.endIgnoringInteractionEvents()
-            if response == nil {
-                print("Error")
-            } else {
-                //Remove annotations
-                let annotations = self.mapView.annotations
-                self.mapView.removeAnnotations(annotations)
-                
-                //Getting Data
-                let latitude = response?.boundingRegion.center.latitude
-                let longitude = response?.boundingRegion.center.longitude
-                
-                //Create Annotation
-                let annotation = MKPointAnnotation()
-                annotation.title = searchBar.text
-                annotation.coordinate = CLLocationCoordinate2DMake(latitude!, longitude!)
-                self.mapView.addAnnotation(annotation)
-                
-                //Zoom
-                let coordinate: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude!, longitude!)
-                let span = MKCoordinateSpan(latitudeDelta: 0.012, longitudeDelta: 0.012)
-                let region = MKCoordinateRegion(center: coordinate, span: span)
-                self.mapView.setRegion(region, animated: true)
-                
-            }
-        }
-        
-    }
-    
     
     
 }
---
+++ EstacionApp/EstacionApp/Controllers/RegisterLocationSearchTable.swift
@@ -0,0 +1,55 @@
+//
+//  RegisterLocationSearchTable.swift
+//  EstacionApp
+//
+//  Created by Ernesto Daniel Mejia Valdiviezo on 1/28/19.
+//  Copyright © 2019 Ernesto Daniel Mejia Valdiviezo. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class RegisterLocationSearchTable: UITableViewController {
+    var matchingItems:[MKMapItem] = []
+    var mapView: MKMapView? = nil
+    var handleResultsSearchDelegate:HandleResultsSearch? = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return matchingItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "registerCell")!
+        let selectedItem = matchingItems[indexPath.row].placemark
+        cell.textLabel?.text = selectedItem.name
+        cell.detailTextLabel?.text = ""
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedItem = matchingItems[indexPath.row].placemark
+        handleResultsSearchDelegate?.dropValueOf(placemark: selectedItem)
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+extension RegisterLocationSearchTable : UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let searchBarText = searchController.searchBar.text else { return }
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = searchBarText
+        request.region = .init()
+        let search = MKLocalSearch(request: request)
+        search.start { response, _ in
+            guard let response = response else { return }
+            self.matchingItems = response.mapItems
+            self.tableView.reloadData()
+        }
+    }
+    
+}
