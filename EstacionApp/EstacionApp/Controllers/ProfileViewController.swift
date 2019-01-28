c68bd70406b8d62399ed9ad820e233c5e696ce06
CobainJosue <chivasagrada@live.com.mx>
2019-01-28 15:11:56 -0600

    ProfileView can update in database the user info

--- EstacionApp/EstacionApp/Controllers/ProfileViewController.swift
+++ EstacionApp/EstacionApp/Controllers/ProfileViewController.swift
@@ -14,13 +14,29 @@ class ProfileViewController: UIViewController {
     
     @IBOutlet weak var scrollView: UIScrollView!
     
+    ///Labels
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var lastnameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    
+    //Text fields
+    @IBOutlet weak var newNameTF: UITextField!
+    @IBOutlet weak var lastnameTF: UITextField!
+    @IBOutlet weak var phoneTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
     
     var user: User?
+    let alert = Alert()
+    var database = Database()
 
     override func viewWillAppear(_ animated: Bool) {
         //Observers para el teclado
         NotificationCenter.default.addObserver(self, selector: #selector(self.adjustForKeyboard(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(self.adjustForKeyboard(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        
         
         super.viewWillAppear(animated)
     }
@@ -32,7 +48,23 @@ class ProfileViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("View did load")
+        
+        database.readFromDatabase { (result: User) in
+            self.user = result
+            DispatchQueue.main.async {
+                self.chargeLabels()
+            }
+        }
 
+    }
+    
+    func chargeLabels() {
+        nameLabel.text = user?.name
+        lastnameLabel.text = user?.lastname
+        emailLabel.text = user?.email
+        phoneLabel.text = user?.phone
     }
     
     @objc func adjustForKeyboard(notification: Notification) {
@@ -68,7 +100,75 @@ class ProfileViewController: UIViewController {
         self.view.endEditing(true)
     }
     
+    @IBAction func changeBtnPressed(_ sender: UIButton) {
+        
+        verifyTF(buttonPressed: sender.tag)
+        
+    }
     
+    func verifyTF(buttonPressed: Int) {
+        
+        let persistent = Persistent()
+        let (_, password) = persistent.readSession("username", "password")
+        
+        switch buttonPressed {
+        case 0:
+            //name
+            if newNameTF.text != "" {
+                let newName = newNameTF.text!
+                update(field: "names", oldValue: nameLabel.text!, newValue: newName)
+            } else {
+                self.present(alert.missingFieldsAlert("Error", "Ingresa un nuevo nombre primero."), animated: true, completion: nil)
+            }
+            break
+        case 1:
+            //lastname
+            if lastnameTF.text != "" {
+                let newLastname = lastnameTF.text!
+                update(field: "lastnames", oldValue: lastnameLabel.text!, newValue: newLastname)
+            } else {
+                self.present(alert.missingFieldsAlert("Error", "Ingresa un nuevo nombre primero."), animated: true, completion: nil)
+            }
+            break
+        case 2:
+            //phone
+            if phoneTF.text != "" {
+                let newPhone = phoneTF.text!
+                update(field: "phone", oldValue: phoneLabel.text!, newValue: newPhone)
+            } else {
+                self.present(alert.missingFieldsAlert("Error", "Ingresa un nuevo nombre primero."), animated: true, completion: nil)
+            }
+            break
+        case 3:
+            //password
+            if passwordTF.text != "" {
+                let newPassword = passwordTF.text!
+                update(field: "password", oldValue: password!, newValue: newPassword)
+            } else {
+                self.present(alert.missingFieldsAlert("Error", "Ingresa un nuevo nombre primero."), animated: true, completion: nil)
+            }
+            break
+        default:
+            break
+            
+        }
+    }
     
+    func update(field: String, oldValue: String, newValue: String) {
+        
+        
+        database.updateInDatabase(field: field, from: oldValue, to: newValue) { (result: Bool) in
+            
+            if result {
+                print("Base actualizada en el campo \(field).")
+            } else {
+                print("Error actualizando datos en base.")
+            }
+            self.present(self.alert.infoUpdate("Informacion actualizada", "Volveras a la pantalla anterior", self), animated: true)
+            self.navigationController?.popViewController(animated: true)
+        }
+        
+        
+    }
 
 }
