b01e8ed59ec702274f1433ce57414bd85005e12c
Ernesto Daniel Mejia Valdiviezo <netosaurio11@MacBook-Air-de-Ernesto.local>
2019-02-02 14:40:47 -0600

    Se completa request para poner marcadoresen la pantalla de home

--- EstacionApp/EstacionApp/Controllers/HomeViewController.swift
+++ EstacionApp/EstacionApp/Controllers/HomeViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import Firebase
 
 protocol HandleMapSearch {
     func dropPinZoomIn(placemark:MKPlacemark)
@@ -15,13 +16,29 @@ protocol HandleMapSearch {
 
 class HomeViewController: UIViewController, UISearchBarDelegate, HandleMapSearch {
 
+  @IBOutlet weak var mapView: MKMapView!
     
     var resultSearchController:UISearchController? = nil
     var selectedPin:MKPlacemark? = nil
-  @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+    var db: Firestore!
+    var annotations: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // [START setup]
+        let settings = FirestoreSettings()
+        
+        Firestore.firestore().settings = settings
+        // [END setup]
+        db = Firestore.firestore()
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+        
+        gettingAnotation()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -42,7 +59,7 @@ class HomeViewController: UIViewController, UISearchBarDelegate, HandleMapSearch
         // cache the pin
         selectedPin = placemark
         // clear existing pins
-        mapView.removeAnnotations(mapView.annotations)
+        //mapView.removeAnnotations(mapView.annotations)
         let annotation = MKPointAnnotation()
         annotation.coordinate = placemark.coordinate
         annotation.title = placemark.name
@@ -51,7 +68,7 @@ class HomeViewController: UIViewController, UISearchBarDelegate, HandleMapSearch
             annotation.subtitle = "\(city) \(state)"
         }
         mapView.addAnnotation(annotation)
-        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let span = MKCoordinateSpan(latitudeDelta: 0.00625, longitudeDelta: 0.00625)
         let region = MKCoordinateRegion(center: placemark.coordinate, span: span)
         mapView.setRegion(region, animated: true)
     }
@@ -73,5 +90,53 @@ class HomeViewController: UIViewController, UISearchBarDelegate, HandleMapSearch
         
     }
     
+    func gettingAnotation(){
+        let usersRef = db.collection("users")
+        usersRef.whereField("address", isGreaterThanOrEqualTo: "")
+            .getDocuments() { [unowned self] (querySnapshot, err) in
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                } else {
+                    for document in querySnapshot!.documents {
+                        self.putAnnotationFor(document.data()["address"]! as! String, document.data()["names"]! as! String )
+                    }
+                }
+        }
+    }
     
+    func putAnnotationFor(_ address: String, _ title: String){
+        let annotation = MKPointAnnotation()
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = address
+        request.region = mapView.region
+        let search = MKLocalSearch(request: request)
+        search.start {[unowned self] response, _ in
+            guard let response = response else { return }
+            annotation.coordinate = response.mapItems[0].placemark.coordinate
+            annotation.title = title
+            annotation.subtitle = response.mapItems[0].placemark.title
+            self.mapView.addAnnotation(annotation)
+        }
+    }
+    
+}
+
+extension HomeViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("error:: \(error.localizedDescription)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.requestLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            let span = MKCoordinateSpan(latitudeDelta: 0.00625, longitudeDelta: 0.00625)
+            let region = MKCoordinateRegion(center: location.coordinate, span: span)
+            mapView.setRegion(region, animated: true)
+        }
+    }
 }
